d5ba363309fb1da4a3d8fb270b05e797879f396d
jaemin <youjmen15@gmail.com>
2021-08-23 18:03:10 +0900

    [ADD] 레이아웃

--- app/src/main/java/com/jaemin/androidfeaturecollection/PreventScreenshotExampleActivity.kt
+++ app/src/main/java/com/jaemin/androidfeaturecollection/PreventScreenshotExampleActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
 
-class PreventScreenshotExample : AppCompatActivity() {
+class PreventScreenshotExampleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_prevent_screenshot_example)
